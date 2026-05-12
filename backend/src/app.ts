6366086693c37d2742d37cfffc1b06058717615e
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-12 03:12:27 +0300

    Add user routes integration in the main application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import userRoutes from "./routes/user.route";
 import cors from "cors";
 
 const app = express();
@@ -6,5 +7,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors());
-
+app.use("/api/users/", userRoutes);
 export default app;
