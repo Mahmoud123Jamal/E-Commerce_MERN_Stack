07b9a93b277ec23616441c1a66add502ed289b70
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-17 06:09:09 +0300

    feat: implement login functionality with user authentication and notifications

--- frontend/e-commerce-platform/src/pages/Login.tsx
+++ frontend/e-commerce-platform/src/pages/Login.tsx
@@ -1,5 +1,50 @@
+import { useNavigate } from "react-router-dom";
+
+import AuthWrapper from "../components/AuthWrapper";
+import InputForm from "../components/InputForm";
+
+import { loginUser } from "../features/auth/authService";
+
+import { login } from "../features/auth/authSlice";
+
+import { useAppDispatch } from "../hooks/reduxHooks";
+
+import type { LoginFormData } from "../types/FormDataType";
+
+import { toast } from "react-toastify";
+
 function Login() {
-  return <div>Login</div>;
+  const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (data: LoginFormData) => {
+    try {
+      const res = await loginUser({
+        email: data.email,
+        password: data.password,
+      });
+
+      dispatch(login({ user: res.data.user, token: res.data.token }));
+
+      toast.success("تم تسجيل الدخول بنجاح!");
+
+      navigate("/");
+    } catch (error) {
+      toast.error("حدث خطأ أثناء تسجيل الدخول.");
+    }
+  };
+
+  return (
+    <AuthWrapper>
+      <InputForm<LoginFormData>
+        headerText="تسجيل الدخول"
+        buttonText="تسجيل الدخول"
+        showName={false}
+        onSubmit={onSubmit}
+      />
+    </AuthWrapper>
+  );
 }
 
 export default Login;
