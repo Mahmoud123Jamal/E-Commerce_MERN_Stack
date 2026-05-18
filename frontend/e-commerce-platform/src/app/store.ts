9510f36a7641716baea97601ee486862f789a05f
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-18 09:24:26 +0300

    feat: integrate langReducer into the store for language state management

--- frontend/e-commerce-platform/src/app/store.ts
+++ frontend/e-commerce-platform/src/app/store.ts
@@ -2,38 +2,24 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import { persistStore, persistReducer } from "redux-persist";
 
-import authReducer from "../features/auth/authSlice";
-import themeReducer from "../features/theme/themeSlice";
-
 import storage from "./storage";
 
-// =======================
-// Root Reducer
-// =======================
+import authReducer from "../features/auth/authSlice";
+import themeReducer from "../features/theme/themeSlice";
+import langReducer from "../features/lang/langSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   theme: themeReducer,
+  lang: langReducer,
 });
-
-// =======================
-// Persist Config
-// =======================
 
 const persistConfig = {
   key: "root",
   storage,
 };
 
-// =======================
-// Persisted Reducer
-// =======================
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// =======================
-// Store
-// =======================
 
 export const store = configureStore({
   reducer: persistedReducer,
@@ -44,15 +30,7 @@ export const store = configureStore({
     }),
 });
 
-// =======================
-// Persistor
-// =======================
-
 export const persistor = persistStore(store);
-
-// =======================
-// Types
-// =======================
 
 export type RootState = ReturnType<typeof store.getState>;
 
