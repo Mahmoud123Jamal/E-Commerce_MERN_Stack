f1108908cf4710d4ed0a87b67172c535dcd112ce
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-19 23:33:46 +0300

    feat: extend User model to include ObjectId type for user identification

--- backend/src/types/User.model.type.ts
+++ backend/src/types/User.model.type.ts
@@ -1,7 +1,12 @@
-import { Document } from "mongoose";
+import { Document, Types } from "mongoose";
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
   role: "user" | "admin";
 }
+
+export type UserType = {
+  _id: Types.ObjectId;
+  name: string;
+};
