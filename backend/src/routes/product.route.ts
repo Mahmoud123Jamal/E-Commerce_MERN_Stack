e0ed5465959a229b8c4ea94d11dfcc595adc1baf
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-19 14:12:07 +0300

    feat: refactor product routes for improved structure and add comment functionality

--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -1,17 +1,39 @@
-import { Router } from "express";
-import {
-  createProduct,
-  updateProduct,
-  deleteProduct,
-  getProduct,
-  getAllProduct,
-} from "../controllers/product.controller";
+import express from "express";
 
-const router = Router();
-router.post("/addProduct", createProduct);
-router.put("/updateProduct/:id", updateProduct);
-router.delete("/deleteProduct/:id", deleteProduct);
-router.get("/:id", getProduct);
-router.get("/getAllProducts", getAllProduct);
+import upload from "../middlewares/upload.middleware";
+
+import {
+  addComment,
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getProductComments,
+  getSingleProduct,
+  updateProduct,
+} from "../controllers/product.controller";
+import { protect } from "../middlewares/auth.middleware";
+import { authRole } from "../middlewares/role.middleware";
+
+const router = express.Router();
+
+router.get("/", getAllProducts);
+router.post(
+  "/",
+  upload.array("images", 5),
+  protect,
+  authRole("admin"),
+  createProduct,
+);
+router.get("/:id", getSingleProduct);
+router.patch(
+  "/:id",
+  protect,
+  authRole("admin"),
+  upload.array("images", 5),
+  updateProduct,
+);
+router.delete("/:id", protect, authRole("admin"), deleteProduct);
+router.get("/:id/comments", getProductComments);
+router.post("/:id/comment", protect, addComment);
 
 export default router;
