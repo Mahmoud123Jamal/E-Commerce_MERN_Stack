07abe1499791900f1746c3416bdac66cc4e52866
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-19 11:10:13 +0300

    feat: expand IProduct interface to include comments and related properties

--- backend/src/types/product.model.type.ts
+++ backend/src/types/product.model.type.ts
@@ -1,6 +1,39 @@
+import { Document, Types } from "mongoose";
+
+export interface IComment {
+  user: Types.ObjectId;
+
+  userName: string;
+
+  comment: string;
+
+  rating: number;
+
+  createdAt: Date;
+}
+
 export interface IProduct extends Document {
   name: string;
+
   description: string;
+
   price: number;
+
   stock: number;
+
+  category: string;
+
+  imageUrl: string;
+
+  multipleImages?: string[];
+
+  comments: IComment[];
+
+  averageRating: number;
+
+  reviewsCount: number;
+
+  createdAt: Date;
+
+  updatedAt: Date;
 }
