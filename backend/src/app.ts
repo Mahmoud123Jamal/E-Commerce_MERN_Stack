78b9c4a0a1d8e853b50dcc93354abba0af7d0aa5
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-19 14:12:23 +0300

    feat: update static file serving to use uploads directory for product images

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,18 @@ import express from "express";
 import userRoutes from "./routes/user.route";
 import productRoutes from "./routes/product.route";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+
+//STATIC FILES
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "../uploads/products")),
+);
+
 app.use(cors());
 app.use("/api/users/", userRoutes);
 app.use("/api/products/", productRoutes);
