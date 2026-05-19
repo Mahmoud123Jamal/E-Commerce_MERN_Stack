f8e58fede95f8e6a9d32fc74af4e32f7521015f6
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-19 22:40:25 +0300

    feat: reorganize product route middleware for improved validation flow

--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -13,27 +13,51 @@ import {
 } from "../controllers/product.controller";
 import { protect } from "../middlewares/auth.middleware";
 import { authRole } from "../middlewares/role.middleware";
+import {
+  addCommentValidator,
+  createProductValidator,
+  updateProductValidator,
+} from "../validators/product.validator";
+import { validationMiddleware } from "../middlewares/validation.middleware";
 
 const router = express.Router();
+/* ================= PRODUCTS ================= */
 
 router.get("/", getAllProducts);
+
+router.get("/:id", getSingleProduct);
+
 router.post(
   "/",
-  upload.array("images", 5),
   protect,
   authRole("admin"),
+  upload.array("images", 5),
+  createProductValidator,
+  validationMiddleware,
   createProduct,
 );
-router.get("/:id", getSingleProduct);
+
 router.patch(
   "/:id",
   protect,
   authRole("admin"),
   upload.array("images", 5),
+  updateProductValidator,
+  validationMiddleware,
   updateProduct,
 );
+
 router.delete("/:id", protect, authRole("admin"), deleteProduct);
+
+/* ================= COMMENTS ================= */
+
+router.post(
+  "/:id/comment",
+  protect,
+  addCommentValidator,
+  validationMiddleware,
+  addComment,
+);
 router.get("/:id/comments", getProductComments);
-router.post("/:id/comment", protect, addComment);
 
 export default router;
