751583cf842bffbd9f545d7e51636a7f39f3ba03
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-19 11:10:34 +0300

    feat: refactor product model to include comments and enhance schema structure

--- backend/src/models/product.model.ts
+++ backend/src/models/product.model.ts
@@ -1,12 +1,95 @@
 import mongoose, { Schema } from "mongoose";
-import { IProduct } from "../types/product.model.type";
 
-const ProductSchema: Schema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  stock: { type: Number, required: true },
-});
-const Product = mongoose.model<IProduct>("Product", ProductSchema);
+import type { IComment, IProduct } from "../types/product.model.type";
 
-export default Product;
+const commentSchema = new Schema<IComment>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    userName: {
+      type: String,
+      required: true,
+    },
+
+    comment: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const productSchema = new Schema<IProduct>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+
+    multipleImages: {
+      type: [String],
+      default: [],
+    },
+
+    comments: {
+      type: [commentSchema],
+      default: [],
+    },
+
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+
+    reviewsCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const ProductModel = mongoose.model<IProduct>("Product", productSchema);
+
+export default ProductModel;
