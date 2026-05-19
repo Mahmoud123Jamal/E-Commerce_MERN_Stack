aad12d23581c259e5d3a1051e7e2c8dbe081eaf1
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-19 11:09:47 +0300

    feat: remove unused bcryptjs import from user model

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,6 +1,5 @@
 import { IUser } from "./../types/User.model.type";
 import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcryptjs";
 const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
