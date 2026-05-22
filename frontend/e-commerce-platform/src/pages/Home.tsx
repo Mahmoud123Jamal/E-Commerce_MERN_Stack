ddf11874a4e9bd49adab36fab73b75256c0465fd
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-22 09:18:51 +0300

    feat: implement Home component layout with swiper and product sections

--- frontend/e-commerce-platform/src/pages/Home.tsx
+++ frontend/e-commerce-platform/src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import { useTranslation } from "react-i18next";
+import Divider from "../components/Divider";
+import HeroSection from "../components/Hero";
+import { HomePageSwiper } from "../components/HomePageSwiper";
+import TopRatedProducts from "../components/TopRatedProducts";
+
 function Home() {
-  return <div>Home</div>;
+  const { i18n } = useTranslation();
+  return (
+    <div>
+      <HomePageSwiper key={i18n.language} />
+      <Divider />
+      <HeroSection />
+      <Divider />
+      <TopRatedProducts />
+    </div>
+  );
 }
 
 export default Home;
