22fb98bf491d458942f5b1fa55458a6e09ba68ab
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-23 15:49:09 +0300

    feat: add useHeroBanners hook for dynamic banner management and internationalization support

---
+++ frontend/e-commerce-platform/src/hooks/useHeroBanners.ts
@@ -0,0 +1,28 @@
+import { useTranslation } from "react-i18next";
+import banner1 from "../assets/banners/banner-slide1.jpg";
+import banner2 from "../assets/banners/banner-slide2.jpg";
+import banner3 from "../assets/banners/banner-slide3.jpg";
+
+export function useHeroBanners() {
+  const { t } = useTranslation();
+
+  const heroBanners = [
+    {
+      image: banner1,
+      title: t("banners.banner1.title"),
+      description: t("banners.banner1.description"),
+    },
+    {
+      image: banner2,
+      title: t("banners.banner2.title"),
+      description: t("banners.banner2.description"),
+    },
+    {
+      image: banner3,
+      title: t("banners.banner3.title"),
+      description: t("banners.banner3.description"),
+    },
+  ];
+
+  return heroBanners;
+}
