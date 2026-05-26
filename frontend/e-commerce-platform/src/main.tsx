d5294852278e5f4ca55c3d39ecefa8c36415a5e0
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-05-26 07:44:00 +0300

    feat: wrap App component with ErrorBoundary for improved error handling

--- frontend/e-commerce-platform/src/main.tsx
+++ frontend/e-commerce-platform/src/main.tsx
@@ -11,6 +11,8 @@ import { store, persistor } from "./app/store";
 import "./i18n";
 import { PersistGate } from "redux-persist/integration/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from "./components/ErrorBoundary.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,7 +29,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <ErrorBoundary fallback={<ErrorFallback />}>
+            <App />
+          </ErrorBoundary>
         </QueryClientProvider>
       </PersistGate>
     </Provider>
